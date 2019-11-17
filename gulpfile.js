076bdcc276bf412f82184850ace37adece906872
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2019-11-17 15:28:54 -0500

    After install of npm modules and jasmine init

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require("gulp");
+var ts = require("gulp-typescript");
+var jasmine = require("gulp-jasmine");
+var merge = require("merge-stream");
+
+gulp.task('ts', function () {
+	var stream1 = gulp.src("src/**/*.ts")
+		.pipe(ts({
+			module: "commonjs",
+			target: "es5"
+		}))
+		.pipe(gulp.dest("src"));
+
+	var stream2 = gulp.src("spec/**/*.ts")
+		.pipe(ts({
+			module: "commonjs",
+			target: "es5"
+		}))
+		.pipe(gulp.dest("spec"));
+
+	return merge(stream1, stream2);
+});
+
+gulp.task('test', gulp.series('ts', function () {
+	return gulp.src('spec/**/*.spec.js')
+		.pipe(jasmine()).on('error', function () { });
+}));
+
+gulp.task('watch', function () {
+	gulp.watch(['spec/**/*.spec.ts','src/**/*.ts']).on('change', gulp.series('test'));
+});
